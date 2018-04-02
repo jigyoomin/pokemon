9414ef75899afb9bdbac31607febbcd24530525b
ji-gyoomin <earth1223@sk.com>
2018-04-02 19:11:31 +0900

    split test

--- src/main/java/poke/dot/ImageExtractor.java
+++ src/main/java/poke/dot/ImageExtractor.java
@@ -2,6 +2,9 @@ package poke.dot;
 
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.SampleModel;
 import java.io.File;
 import java.io.IOException;
 
@@ -23,15 +26,60 @@ public class ImageExtractor {
 		
 		ImageExtractor extractor = new ImageExtractor();
 		BufferedImage image = extractor.loadImage(imagePath);
-		extractor.split(image);
-//		extractor.display(image);
+		BufferedImage split = extractor.split(image);
+		extractor.display(split);
 	}
 	
-	private void split(BufferedImage image) {
-		for (int i = 0 ; i < 100 ; i++) {
-			int rgb = image.getRGB(i, i);
-			System.out.println(String.format("%d : %d - %d", i, i, rgb));
+	private BufferedImage split(BufferedImage image) {
+		
+//		for (int i = 0 ; i < 100 ; i++) {
+//			int rgb = image.getRGB(i, i);
+//			int a = rgb >> 32 & 0xff;
+//			int r = rgb >> 16 & 0xff;
+//			int g = rgb >> 8 & 0xff;
+//			int b = rgb  & 0xff;
+//			
+//			System.out.println(String.format("%d : %d - %d %d %d", i, i, r, g, b));
+//		}
+		
+		int[][] split = new int[100][];
+		int hIndex = 0;
+		// 0 - prepare
+		// 1 - start
+		int status = 0;
+		int subHeight = 0;
+		for (int h = 0 ; h < image.getHeight() ; h++) {
+			int[] line = new int[image.getWidth()];
+			boolean isNull = true;
+			for (int w = 0 ; w < image.getWidth() ; w++) {
+				line[w] = image.getRGB(w, h);
+				if (line[w] != 0) isNull = false;
+				
+			}
+			if (!isNull) {
+				split[hIndex++] = line;
+				subHeight++;
+				status = 1;
+			} else {
+				if (status == 1) {
+					status = 0;
+					// 한무더기 끝
+					// subHeight = 0;
+					break;
+				}
+			}
 		}
+		
+		BufferedImage resultImage = new BufferedImage(image.getWidth(), subHeight, image.getType());
+		for (int i = 0 ; i < split.length ; i++) {
+			if (split[i] == null) {
+				break;
+			}
+			for (int j = 0 ; j < image.getWidth() ; j++) {
+				resultImage.setRGB(j, i, split[i][j]);
+			}
+		}
+		return resultImage;
 	}
 	
 	private BufferedImage loadImage(String imagePath) throws IOException {
